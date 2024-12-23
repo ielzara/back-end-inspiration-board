e9ac6c6532eb1e17a1e017e5c31dc866642cf24b
somy <smycho28@gmail.com>
2024-12-23 10:11:58 -0800

    renamed paramenters

--- app/models/card.py
+++ app/models/card.py
@@ -2,6 +2,6 @@ from sqlalchemy.orm import Mapped, mapped_column
 from ..db import db
 
 class Card(db.Model):
-    card_id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     message: Mapped[str]
-    likes_count: Mapped[int]
+    count: Mapped[int]
