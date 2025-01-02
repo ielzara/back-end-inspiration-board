5eed7c43d5c169827f618d2fcc4dbff1b3409088
somy <smycho28@gmail.com>
2025-01-01 20:16:19 -0800

    added to_dict, from_dict, and routes

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from flask_cors import CORS
 from .db import db, migrate
-from .models import board, card
+from .models import board, card  # cards_bp
+from .routes.card_routes import cards_bp
 import os
 # Import models, blueprints, and anything else needed to set up the app or database
 
@@ -10,8 +11,8 @@ def create_app(config=None):
     app = Flask(__name__)
 
     app.config["CORS_HEADERS"] = "Content-Type"
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("SQLALCHEMY_DATABASE_URI")
 
     if config:
         app.config.update(config)
@@ -20,9 +21,9 @@ def create_app(config=None):
     migrate.init_app(app, db)
     # Initialize app with SQLAlchemy db and Migrate
 
-    # Register Blueprints 
+    # Register Blueprints
     # app.register_blueprint(board_bp)
-    # app.register_blueprint(card_bp)
+    app.register_blueprint(cards_bp)
 
     CORS(app)
     return app
--- app/routes/card_routes.py
+++ app/routes/card_routes.py
@@ -1,6 +1,65 @@
-from flask import Blueprint, abort, make_response, request, Response
+# from flask import Blueprint, abort, make_response, request, Response
+# from app.models.card import Card
+# import os
+# import requests
+# from ..db import db
+# from .route_utilities import validate_model, create_model
+from flask import Blueprint, abort, make_response, request
 from app.models.card import Card
+from ..db import db
 import os
 import requests
-from ..db import db
 from .route_utilities import validate_model, create_model
+
+cards_bp = Blueprint("cards_bp", __name__, url_prefix="/cards")
+
+@cards_bp.post("")
+def create_card():
+
+    request_body = request.get_json()
+
+    # Check for required fields before calling create_model
+    if "message" not in request_body or "count" not in request_body:
+        return make_response({"details": "Invalid data"}, 400)
+
+    response = create_model(Card, request_body)
+    card_dict = response.get_json()
+    return make_response({"Card": card_dict}, 201)
+
+    
+@cards_bp.get("")
+def get_all_books():
+    query = db.select(Card).order_by(Card.id)
+    cards = db.session.scalars(query)
+    # We could also write the line above as:
+    # books = db.session.execute(query).scalars()
+
+    cards_response = []
+    for card in cards:
+        cards_response.append(
+            {
+                "id": card.id,
+                "message": card.title,
+                "count": card.description
+            }
+        )
+    return cards_response
+
+@cards_bp.get("/<card_id>")
+def get_one_card(card_id):
+    query = db.select(Card).where(Card.id == card_id)
+    card = db.session.scalar(query)
+
+    card = validate_model(Card, card_id)
+    return {"card": card.to_dict()}, 200
+
+# DELETE
+@cards_bp.delete("/<card_id>")
+def delete_card(card_id):
+    card = validate_model(Card, card_id)
+    card_message = card.message
+    db.session.delete(card)
+    db.session.commit()
+
+    return {"details": f'Card {card_id} "{card_message}" successfully deleted'}, 200
+
